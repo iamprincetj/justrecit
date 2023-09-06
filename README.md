cbd06c45a1f7f159b623774817903b31e5884853
iamprincetj <tochukwunwanze5@gmail.com>
2023-09-06 12:47:39 +0100

    updated

--- README.md
+++
@@ -1,7 +0,0 @@
-JustReciT Portfolio project for ALX Africa
-
-JustReciT is a site for recommending similra tracks based on what the user searches for
-
-Why listen to just one song when you can find similar ones and keep the mood
-going? With our easy-to-use interface,
-you can find what you're looking for in no time
---
+++ index1.js
@@ -0,0 +1,26 @@
+
+let getSearchItem = document.querySelector("#search-item");
+let getSearchBtn = document.querySelector("#search-btn");
+
+getSearchItem.style.visibility = "hidden";
+getSearchItem.addEventListener("keyup", (e)=> {
+    if (e.keyCode === 13) {
+
+        if (getSearchItem.value) {
+            sessionStorage.setItem("searchItem", getSearchItem.value);
+            sessionStorage.setItem("currentPage", 1);
+            window.location.href = "landingpage.html"
+        }else {
+            alert("Please enter a search item");
+        }
+    }
+});
+if (getSearchBtn) {
+    getSearchBtn.addEventListener("click", ()=> {
+        getSearchItem.style.visibility = "visible";
+        getSearchItem.focus();
+        getSearchBtn.style.visibility = "hidden";
+    });
+}else {
+    getSearchItem.style.visibility = "visible";
+}
